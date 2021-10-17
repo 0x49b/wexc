0ab18628cf1b3ca2f37954866bb1a91fab3b699a
Luca Dietiker <50096011+dietikerluca@users.noreply.github.com>
2021-10-17 17:18:13 +0200

    add minute slot arc

--- clock.js
+++ clock.js
@@ -1,6 +1,16 @@
 const canvas = document.getElementById("canvas");
 const cx = canvas.getContext("2d");
 
+const lightColor = '#ADCEFF'
+const darkColor = '#4485E8'
+
+const centerX = canvas.width / 2;
+const centerY = canvas.height / 2;
+
+const lineWidth = 55;
+const handleLength = 8;
+const radius = canvas.width / 2 - lineWidth - 10;
+
 function start() {
     nextClock()
     setInterval(() => {
@@ -11,26 +21,27 @@ function start() {
 function nextClock() {
     cx.clearRect(0, 0, 600, 600)
     clockFace();
-    drawArc(7, 0, 10, 55, '#ADCEFF', true);
-    drawArc(7, 50, 10, 00, '#4485E8', false);
+    drawOuterArc(7, 0, 10, 55, lightColor, true);
+    drawOuterArc(7, 50, 10, 0, darkColor, false);
+    drawInnerArc( 50,  55, lightColor, true)
     drawHourLabels();
     drawMiddlePoint();
 }
 
 const getRadians = (degree) => (degree * Math.PI) / 180
 
-function drawHourLabels(){
+function drawHourLabels() {
     // Stunden Labels
     let labelAngle = -75;
     const labelRadius = 235;
     const fontSize = 30;
-    const xCorrex = -(fontSize/2); // Because of the FontSize to position it correct over the Strokes
-    const yCorrex = (fontSize/3);
+    const xCorrex = -(fontSize / 2); // Because of the FontSize to position it correct over the Strokes
+    const yCorrex = (fontSize / 3);
     for (let i = 0; i < 24; i++) {
         cx.save();
         cx.translate(300, 300);
         cx.fillStyle = "#6D6D6D";
-        cx.font = '300 '+fontSize+'px Roboto';
+        cx.font = '300 ' + fontSize + 'px Roboto';
         let x = labelRadius * Math.cos(getRadians(labelAngle + (15 * i))) + xCorrex
         let y = labelRadius * Math.sin(getRadians(labelAngle + (15 * i))) + yCorrex
         cx.fillText((1 + i).toString(), x, y);// Text
@@ -53,8 +64,6 @@ function clockFace() {
     cx.fill();
     cx.closePath();
     cx.restore();
-
-
 
 
     // Stroke - Skala
@@ -80,7 +89,7 @@ function clockFace() {
 
 }
 
-function drawMiddlePoint(){
+function drawMiddlePoint() {
     // Punkt in der Mitte
     cx.fillStyle = '#606060';
     cx.save();
@@ -94,11 +103,29 @@ function drawMiddlePoint(){
 }
 
 
-
-function angleForTime(hours, minutes) {
+/**
+ * @param {number} hours
+ * @param {number} minutes
+ * @param {boolean} clockFaceMinutes – if true, the calculated angle applies to a clock face with minutes only.
+ */
+function angleForTime(hours, minutes, clockFaceMinutes = false) {
     const fullCircleAngle = 2 * Math.PI;
-    let angleOfMinute = fullCircleAngle / (24 * 60);
-    return (hours * 60 + minutes) * angleOfMinute;
+    const totalMinutes = clockFaceMinutes ? minutes : (hours * 60 + minutes)
+    let angleOfMinute;
+
+    if (clockFaceMinutes) {
+        angleOfMinute = fullCircleAngle / 60;
+    } else {
+        angleOfMinute = fullCircleAngle / (24 * 60);
+    }
+
+    let angle = totalMinutes * angleOfMinute;
+
+    // move start of 0° to top
+    let angle45Degree = 0.5 * Math.PI;
+    angle -= angle45Degree;
+
+    return angle
 }
 
 function drawLine(x, y, radius, angle, length, color) {
@@ -112,31 +139,47 @@ function drawLine(x, y, radius, angle, length, color) {
     cx.stroke();
 }
 
-function drawArc(startHour, startMinute, endHour, endMinute, color, drawLines) {
-    // center position
-    let x = canvas.width / 2;
-    let y = canvas.height / 2;
+function drawOuterArc(startHour, startMinute, endHour, endMinute, color, drawLines = false) {
 
-    let lineWidth = 55;
-    let radius = canvas.width / 2 - lineWidth -10;
     let startAngle = angleForTime(startHour, startMinute);
     let endAngle = angleForTime(endHour, endMinute);
 
-    // move start of 0° to top
-    let angle45Degree = 0.5 * Math.PI;
-    startAngle = startAngle - angle45Degree;
-    endAngle = endAngle - angle45Degree;
-    // draw arc
-    cx.beginPath();
-    cx.arc(x, y, radius, startAngle, endAngle);
-    cx.lineWidth = lineWidth;
-
-    // line color
-    cx.strokeStyle = color;
-    cx.stroke();
+    drawArc(startAngle, endAngle, color, false)
 
     if (drawLines) {
-        drawLine(x, y, radius, endAngle, 20, '#90F0B6');
-        drawLine(x, y, radius, startAngle, 20, '#F09090');
+        drawLine(centerX, centerY, radius, endAngle, 20, '#90F0B6');
+        drawLine(centerX, centerY, radius, startAngle, 20, '#F09090');
+    }
+}
+
+function drawInnerArc(startMinute, endMinute, color, drawLines = true) {
+
+    let startAngle = angleForTime(0, startMinute, true);
+    let endAngle = angleForTime(0, endMinute, true);
+
+    drawArc(startAngle, endAngle, color, true)
+
+    if (drawLines) {
+        drawLine(centerX, centerY, radius-lineWidth+handleLength, endAngle, 20, '#90F0B6');
+        drawLine(centerX, centerY, radius-lineWidth+handleLength, startAngle, 20, '#F09090');
+    }
+}
+
+function drawArc(startAngle, endAngle, color, drawInner = false) {
+
+    cx.beginPath();
+
+    if (drawInner) {
+        // inner
+        if (drawInner) cx.moveTo(centerX, centerY);
+        cx.arc(centerX, centerY, radius-lineWidth, startAngle, endAngle);
+        cx.fillStyle = color;
+        cx.fill();
+    } else {
+        // outer
+        cx.arc(centerX, centerY, radius, startAngle, endAngle);
+        cx.lineWidth = lineWidth;
+        cx.strokeStyle = color;
+        cx.stroke();
     }
 }
